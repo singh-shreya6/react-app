f34f2a2616463bc8ee76b4dcbbcdf37175802c1d
Shreya Singh <singh.shreya8@gmail.com>
2021-07-04 22:36:58 +0530

    React app creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import  get  from 'lodash/get';
+import Table from 'react-bootstrap/Table';
+import forEach from 'lodash/forEach';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ class App extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        bpi: {},
+        time: {}
+      };
+  }
+
+  componentDidMount() {
+    document.title = "Bitcoin Prices";
+    fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+        console.log(data);
+        const bpi = get(data, 'bpi', {});
+        const time = get(data, 'time', {})
+        this.setState({
+          bpi: bpi,
+          time: time
+        });
+    });
+  }
+
+  getTableBody() {
+        let tableData = [];
+        forEach(this.state.bpi, (data, key) => {
+            tableData.push(
+                <tr>
+                    <td>
+                            {data.code}
+                    </td>
+                    <td>
+                            {data.description}
+                    </td>
+                    <td>
+                                {data.rate}
+                    </td>
+                </tr>
+            );
+    });
+        return tableData;
+    }
+
+  getTable() {
+        return (
+            <Table>
+             <thead>
+                <tr>
+                    <th>Country
+                    </th>
+                    <th>Description</th>
+                    <th>Rate</th>
+                </tr>
+             </thead>
+             <tbody>
+                 {this.getTableBody()}
+             </tbody>
+            </Table>
+        );
+    }
+
+  render() {
+    return (
+      <div>
+        <h>Bitcoin prices at {this.state.time.updated}</h>
+        <div className = "price_table">
+        {this.getTable()}
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
